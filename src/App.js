e40bebadb3b3aa45451a8cb2e5ac0dd93c04983a
Prachi Rattan <prachirattan07@gmail.com>
2024-09-16 04:33:32 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Registration from './pages/Registration';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Registration" element={<Registration />}/>
         </Routes>
       </div>
     </Router>
