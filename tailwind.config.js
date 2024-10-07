71555f81ae9e9f2a93cfd2fe8330e1195b92d367
Kritika <kritika.singh1824@gmail.com>
2024-10-07 19:44:43 +0530

    simple changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        '9/16': '56.25%' // 16:9 aspect ratio
+      }
+    },
   },
   plugins: [],
 }
